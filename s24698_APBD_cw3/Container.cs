047a887b7127495d9fb854bdc32539e4ed2b472e
JWilanowski <s24698@pjwstk.edu.pl>
2025-03-25 20:35:29 +0100

    Dodaj pliki projektów.

---
+++ s24698_APBD_cw3/Container.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace s24698_APBD_cw3
+{
+
+
+    public class Container
+    {
+        public double LoadMass { get; set; }
+        public int Height { get; set; }
+        public double OwnMass { get; set; }
+
+    }
+}
---
+++ s24698_APBD_cw3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace s24698_APBD_cw3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
